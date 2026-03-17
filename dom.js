333e1ddbb015232c84e1057c3b80ee6aba3aabee
Ujjwal Bhardwaj <bhardwajujjwal2008@gmail.com>
2026-03-17 16:29:02 +0530

    dom

---
+++ dom.js
@@ -0,0 +1,20 @@
+//  let x= document.getElementById("demo").getAttribute("id");
+
+//  let y= document.getElementById("demo").setAttribute("Class","democlass");
+// //  x.innerHTML =" ";
+// // let y = document.getElementsByClassName("demo");
+// y[0].innerHTML = " hello"
+// let z = document.getElementsByTagName("demo");
+// z[0].innerHTML = "everyone"
+// let q = document.querySelector()
+// styling
+// let y= document.getElementById("demo").style
+// create Element
+// document.createElement("h2")
+// let pox = document.getElementsByTagName
+// mouse Event => onmouseover,onmouseout,onmousedown,onmouseup,onmousemove
+// KeyboardEvent => onkeydown,onkeyup
+// form => onfocus,onsubmit,onblur,onchange
+// window events => onload,onunload,
+<button onclick = "onclick()">Click me</button>
+< button onmouseover ="onmouse"
--- script.js
+++ script.js
@@ -1,2 +1,8 @@
-console.log("Hello World")
-document.write("Hello World")
+// var person2 = new Object();
+// person2.name ='krishkant';
+// person2.age =32;
+// person2.isMarrid = true;
+// person2.address = {};
+// person2.address.street = "nagar road";
+// person2['address']['flatNO'] =33;
+// console.log(person2);
